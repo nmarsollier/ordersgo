a859ddf791e5b1a2ba92f51095996f65c55c1389
Nestor Marsollier <nmarsollier@gmail.com>
2024-08-09 14:01:52 -0300

    Update get_orders.go

--- rest/get_orders.go
+++ rest/get_orders.go
@@ -1,6 +1,8 @@
 package rest
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/nmarsollier/ordersgo/order_proj"
 	"github.com/nmarsollier/ordersgo/rest/engine"
@@ -16,7 +18,7 @@ import (
 //	@Accept			json
 //	@Produce		json
 //	@Param			Authorization	header		string					true	"bearer {token}"
-//	@Success		200				{array}		order_proj.Order		"Ordenes"
+//	@Success		200				{array}		OrderListData		"Ordenes"
 //	@Failure		400				{object}	errors.ErrValidation	"Bad Request"
 //	@Failure		401				{object}	errors.ErrCustom		"Unauthorized"
 //	@Failure		404				{object}	errors.ErrCustom		"Not Found"
@@ -50,5 +52,47 @@ func getOrders(c *gin.Context) {
 		return
 	}
 
-	c.JSON(200, e)
+	orders := []OrderListData{}
+	for _, o := range e {
+		orders = append(orders, OrderListData{
+
+			Id:           o.OrderId,
+			Status:       o.Status,
+			CartId:       o.CartId,
+			TotalPrice:   totalPrice(o),
+			TotalPayment: totalPayment(o),
+			Updated:      o.Updated,
+			Created:      o.Created,
+			Articles:     len(o.Articles),
+		})
+	}
+
+	c.JSON(200, orders)
+}
+
+func totalPayment(order *order_proj.Order) float32 {
+	var result float32 = 0
+	for _, o := range order.Payments {
+		result += float32(o.Amount)
+	}
+	return result
+}
+
+func totalPrice(order *order_proj.Order) float32 {
+	var result float32 = 0
+	for _, o := range order.Articles {
+		result += float32(o.UnitaryPrice) * float32(o.Quantity)
+	}
+	return result
+}
+
+type OrderListData struct {
+	Id           string                 `json:"id"`
+	Status       order_proj.OrderStatus `json:"status"`
+	CartId       string                 `json:"cartId"`
+	TotalPrice   float32                `json:"totalPrice"`
+	TotalPayment float32                `json:"totalPayment"`
+	Updated      time.Time              `json:"updated"`
+	Created      time.Time              `json:"created"`
+	Articles     int                    `json:"articles"`
 }
